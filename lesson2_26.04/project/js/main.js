73b1df3250a4465fdf5a10e579e9675eb669f625
oleg <olegsaltuk@yandex.ru>
2019-04-29 12:10:39 +0300

    lesson2
    task 1-2

---
+++ lesson2_26.04/project/js/main.js
@@ -0,0 +1,112 @@
+class ProductsList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.products = [];
+        this.productsAll = [];
+        this._init();
+    }
+    _init(){
+        this._fetchProducts();
+        this._render();
+    }
+    _fetchProducts(){
+        this.products = [
+            {id: 1, title: 'Notebook', price: 2000},
+            {id: 2, title: 'Mouse', price: 20},
+            {id: 3, title: 'Keyboard', price: 34},
+            {id: 4, title: 'Gamepad', price: 56},
+            {id: 5, title: 'Chair', price: 120},
+        ];
+    }
+    _render(){
+        const block = document.querySelector(this.container);
+        for(let product of this.products){
+            const prodObj = new ProductItem(product);
+            this.productsAll.push(prodObj);
+            block.insertAdjacentHTML('beforeend', prodObj.render());
+        }
+    }
+
+    listTotalPrice(productList = this.products){
+        let total = 0;
+        for (let item of productList){
+            total += item.price;
+        }
+        return total
+    }
+}
+
+class ProductItem {
+    constructor(product, img='https://placehold.it/200x150'){
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+    }
+    render(){
+        return `<div class="product-item">
+                <img src="${this.img}" alt="${this.title}">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price}</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
+            </div>`
+    }
+}
+
+class Cart {
+    constructor(productArr, container = ".cart"){
+
+    }
+    // Создает элемент корзины, добавдяет в массив товаров
+    _createItem(){}
+
+    //Удаляет товар из корзины
+    _removeItem(){}
+
+    //обновляет данные о товарах в корзине
+    _updateCart(){}
+
+    //инициализирует корзину
+    _init(){}
+
+    //отрисовывет корзину на странице
+    _render(){}
+
+    //Считает сумму товаров и кол-во
+    _calcTotal(){}
+
+
+
+}
+const products = new ProductsList();
+
+// const products = [
+//     {id: 1, title: 'Notebook', price: 2000},
+//     {id: 2, title: 'Mouse', price: 20},
+//     {id: 3, title: 'Keyboard', price: 34},
+//     {id: 4, title: 'Gamepad', price: 56},
+//     {id: 5, title: 'Chair', price: 120},
+// ];
+//
+// const renderProduct = (product, img = 'https://placehold.it/200x150') => {
+//     return `<div class="product-item">
+//                 <img src="${img}" alt="${product.title}">
+//                 <div class="desc">
+//                     <h3>${product.title}</h3>
+//                     <p>${product.price}</p>
+//                     <button class="buy-btn">Купить</button>
+//                 </div>
+//             </div>`
+// };
+//
+// const renderPage = (list=products) => {
+//     // document.querySelector('.products').innerHTML = list.map(item => renderProduct(item.title, item.price)).join('');
+//
+//     for(let el of list){
+//         document.querySelector('.products').insertAdjacentHTML('beforeend', renderProduct(el));
+//     }
+// };
+//
+// renderPage();
