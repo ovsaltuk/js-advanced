0de7317c42bdba500b22032381f292e838f44a6f
ovsaltuk <42245416+ovsaltuk@users.noreply.github.com>
2019-05-13 23:07:43 +0300

    3 task
    problem

--- lesson4_07.05/HW/task 3/js/Validator.js
+++ lesson4_07.05/HW/task 3/js/Validator.js
@@ -1,8 +1,9 @@
 class Validator {
-    constructor(regExp, input, message = "error"){
+    constructor(regExp, input, message = "error", button = "button"){
         this.regExp = regExp;
         this.input = input;
         this.message = message;
+        this.button = button;
         this.init();
     }
 
@@ -14,11 +15,31 @@ class Validator {
         return this.regExp.test(this.getInput(this.input).value);
     }
 
-    getMessage(message){
-
+    getMessage(){
+        this.deleteMessage();
+        if(!this.isValid()){
+            this.getInput().insertAdjacentHTML('afterend', `<p class="errorMessage">${this.message}</p>`);
+        }
     }
+
+    deleteMessage(){
+        let messageArr = [...document.querySelectorAll('.errorMessage')];
+        for (let el of messageArr){
+            el.remove();
+        }
+    }
+
+    buttonListener(){
+        let submit = document.querySelector(`.${this.button}`);
+        submit.addEventListener('click', (e)=>{
+            e.preventDefault();
+            this.getMessage();
+        })
+    }
+
     init(){
         this.getInput();
+        this.buttonListener();
     }
 
 }
--- lesson4_07.05/HW/task 3/js/mainTask3.js
+++ lesson4_07.05/HW/task 3/js/mainTask3.js
@@ -1,6 +1,10 @@
-const phone = /[a-z,A-Z]{3,30}|[а-я,А-Я]{3,30}/i;
+
+let name = new Validator(/[a-z,A-Z]{3,30}|[а-я,А-Я]{3,30}/i, 'name',
+    'Имя должно содеражить только буквы');
+
+let phone = new Validator(/\+7\(\d{3}\)\d{3}\-\d{4}/,'phone',
+    'Телефон должен соответствовать формату +7(000)000-0000');
 
 
-
-let name = new Validator(phone, 'name', 'Имя должно содеражить только буквы');
-
+let mail = new Validator(/.+@.+\.(ru|com)/, 'mail',
+    'E-mail имеет вид mymail@mail.ru, или my.mail@mail.ru, или my-mail@mail.ru.');
